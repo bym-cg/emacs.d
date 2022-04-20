442893e6597437956153a9d5198b0803591a3a60
Yves <yves@mail.com>
2022-04-20 07:58:06 +0000

    Update init.el and add emacs-config and myinit.org

--- init.el
+++ init.el
@@ -1,19 +1,56 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(which-key try linum-relative linenum evil)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (setq inhibit-startup-message t)
 
 (require 'package)
-(require 'use-package)
 
-;;(setq package-enable-at-startup nil)
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/"))
-
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+(require 'use-package)
 
+(use-package try
+  :ensure t)
 
+(use-package evil
+  :ensure t)
+
+(evil-mode 1)
+
+(use-package org
+  :ensure t)
+
+(use-package linum-relative
+  :ensure t
+  :after (evil)
+  :config
+  (add-hook 'prog-mode-hook 'linum-on)
+  (set-face-foreground 'linum "#999")
+  (setq linum-format "%d "))
+
+(linum-relative-mode 1)
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
 
